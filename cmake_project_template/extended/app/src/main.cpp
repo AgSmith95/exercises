12845469ce540bd5cb826590224c28b7e6f7b31a
AgSmith95 <ivan.a.kornienko@gmail.com>
2019-05-05 20:48:05 +0300

    moved simple template to different folder; added extended template; added .gitignore files.

---
+++ cmake_project_template/extended/app/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lib.hpp"
+
+int main() {
+    std::cout << lib_foo() << "\n";
+}
---
+++ cmake_project_template/extended/lib/include/lib.hpp
@@ -0,0 +1,6 @@
+#ifndef PROJECT_NAME_LIB_HPP
+#define PROJECT_NAME_LIB_HPP
+
+int lib_foo();
+
+#endif //PROJECT_NAME_LIB_HPP
