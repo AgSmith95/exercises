8d77f279e0ff116c589c42f5a9e44f88ff0b2a1c
AgSmith <ivan.a.kornienko@gmail.com>
2019-09-03 23:03:13 +0300

    functions/inline: added basic example of inline and some explanations to it

---
+++ interview/functions/inline_basics/lib.cpp
@@ -0,0 +1,8 @@
+#include "inline.h"
+#include "lib.h"
+
+int add_all(const int i) {
+    return add5(i)
+//    + add8(i)
+    ;
+}
---
+++ interview/functions/inline_basics/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "inline.h"
+#include "lib.h"
+
+int main() {
+    std::cout << "add5(5)=" << add5(5) << '\n';
+    //std::cout << "add8(5)=" << add8(5) << '\n';
+    std::cout << "add_all(5)=" << add_all(5) << '\n';
+    return 0;
+}
