b141137dd4a6b63e89dbaa67a410b6566fa8a8d8
AgSmith <ivan.a.kornienko@gmail.com>
2020-04-25 01:13:53 +0300

    interview/core/enum_class
    explaining why enum class can be useful
    enum class items exist in separate namespace

--- interview/core/enum_class/main.cpp
+++ interview/core/enum_class/main.cpp
@@ -6,6 +6,29 @@ enum class Color {
     blue
 };
 
+// can define items "same" names
+// due to the fact that Colour is different namespace
+enum class Colour {
+    red,
+    green,
+    blue
+};
+
+enum Count {
+    first,
+    second
+};
+
+// cannot define items with same names using simple enums
+enum Counter {
+//    first   // error: ‘first’ conflicts with a previous declaration
+              // note: previous declaration ‘List1 first’
+//    second, // error: ‘second’ conflicts with a previous declaration
+              // note: previous declaration ‘Count second’
+    First,
+    Second
+};
+
 int main() {
     Color c;
 //    c = red; // error: ‘red’ was not declared in this scope; did you mean ‘Color::red’
@@ -17,6 +40,16 @@ int main() {
 
     c = Color::blue;
     std::cout << static_cast<int>(c) << '\n'; // 2
+
+    Colour C;
+    C = Colour::red;
+    std::cout << static_cast<int>(C) << '\n'; // 0
+
+    C = Colour::green;
+    std::cout << static_cast<int>(C) << '\n'; // 1
+
+    C = Colour::blue;
+    std::cout << static_cast<int>(C) << '\n'; // 2
 
     return 0;
 }
