7ded017c6d7aec801b8d93bfea5f5701606f5bfb
AgSmith <ivan.a.kornienko@gmail.com>
2020-04-25 00:52:15 +0300

    interview/values/lvalues_refs
    honestly, I dont remember why I wrote this in the first place
    added commented output and section numbering

--- interview/values/lrvalues_refs/lrvalues.cpp
+++ interview/values/lrvalues_refs/lrvalues.cpp
@@ -18,40 +18,66 @@
  */
 
 int main() {
+    std::cout << "1\n";
     {
         X lvalue{};
         X &&rvalue_ref{};
 
         lvalue = rvalue_ref; // X copy assigned
+//        X()
+//        X()
+//        X copy assigned
     }
-    std::cout << "\n";
+    std::cout << "\n2\n";
     {
         X &&rvalue_ref{};
         X lvalue{rvalue_ref}; // X copy constructed
         lvalue.use();
+
+//        X()
+//        X copy constructed
     }
-    std::cout << "\n";
+    std::cout << "\n3\n";
     {
         X lvalue{};
         X &&rvalue_ref{};
 
         lvalue = std::move(rvalue_ref); // X move assigned
+
+//        X()
+//        X()
+//        X move assigned
     }
-    std::cout << "\n";
+    std::cout << "\n4\n";
     {
         X &&rvalue_ref{};
         X lvalue{std::move(rvalue_ref)}; // X move constructed
         lvalue.use();
+
+//        X()
+//        X move constructed
     }
-    std::cout << "\n";
+    std::cout << "\n5\n";
     {
         A a;
         no_pf(std::move(a));
+
+//        A()
+//        no_pf(A&& a)
+//        A(const A&)
+//        ~A()
+//        ~A()
     }
-    std::cout << "\n";
+    std::cout << "\n6\n";
     {
         A a;
         pf(std::move(a));
+
+//        A()
+//        pf(A&& a)
+//        A(A&&)
+//        ~A()
+//        ~A()
     }
 
     // see also perfect forwarding
