3b2e427237735dc92fbc832b225d2785d4f33750
AgSmith95 <ivan.a.kornienko@gmail.com>
2020-08-22 14:46:02 +0300

    snippets
    encapsulation "hack"
    modify private fields with the power of pointer arithmetic

---
+++ snippets/encapsulation_hack/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class A {
+public:
+    A(int i, long l): I{i}, L{l} {}
+    int getI() const { return I; }
+    long getL() const { return L; }
+private:
+    int I;
+    long L;
+};
+
+int main() {
+    A a(5,10);
+    std::cout << sizeof(A) << '\n';
+    std::cout << sizeof(char) << '\n';
+    std::cout << sizeof(int) << '\n';
+    std::cout << sizeof(long) << '\n';
+    std::cout << "A(" << a.getI() << ',' << a.getL() << ")\n";
+
+    // A:   16 bytes =
+    //       4 bytes - I
+    //       4 bytes - padding
+    //       8 bytes - L
+    char* hack = (char*)&a;
+    *((int*)hack) = 10;
+    *((long*)(hack+8)) = 15;
+    std::cout << "A(" << a.getI() << ',' << a.getL() << ")\n";
+
+    return 0;
+}
