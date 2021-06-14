fdbec5d1358bd7ee4b6652b1ee9458df70a2265a
AgSmith95 <ivan.a.kornienko@gmail.com>
2021-06-15 01:30:47 +0300

    minmax_elements
    work in progress

--- stepanov_a9/swap_min_search/min.h
+++ stepanov_a9/swap_min_search/min.h
@@ -19,22 +19,4 @@ const T& min(const T& a, const T& b) {
     return ::min(a, b, std::less<T>());
 }
 
-template <typename I, typename Compare>
-// requires I is a ForwardIterator
-// I is not an InputIterator because they only go over the data once
-//     so you can't store values like done here: "I min_el = first;"
-I min_element(I first, I last, Compare cmp) {
-    if (first == last) return last;
-
-    I min_el = first;
-    ++first;
-    while (first != last) {
-        if (cmp(*first, *min_el)) {
-            min_el = first;
-        }
-        ++first;
-    }
-    return min_el;
-}
-
 #endif //MIN_H
