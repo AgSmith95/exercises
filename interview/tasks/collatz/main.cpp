e2400a2e6dbae1a8243296f6bea1958235046787
AgSmith95 <ivan.a.kornienko@gmail.com>
2021-09-14 02:09:16 +0300

    longest collatz
    slightly faster

--- interview/tasks/collatz/main.cpp
+++ interview/tasks/collatz/main.cpp
@@ -2,22 +2,26 @@
 #include <cstdlib>
 
 unsigned long long collatz(unsigned long long n) {
-    //if (n == 0) return 0; // it is an error but ok
-    if (n == 1) return 1;
-    else if (n == 2) return 2;
-
     unsigned long long result = 0;
-    while (n != 4) {
-        ++result;
-        n = (!(n & 1)) ? (n >> 1) : (3*n + 1);
+    while (n != 2) {
+        if (n & 1) {
+            n = (3*n + 1) >> 1;
+            result += 2;
+        }
+        else {
+            n >>= 1;
+            ++result;
+        }
     }
-    result += 3;
+    result += 2;
 
     return result;
 }
 
 unsigned long long longest_collatz_uptoN(unsigned long long n) {
     if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else if (n == 2) return 2;
 
     unsigned long long index = 1;
     unsigned long long max = 1;
--- interview/tasks/collatz/main_readable.cpp
+++ interview/tasks/collatz/main_readable.cpp
@@ -2,7 +2,6 @@
 #include <cstdlib>
 
 unsigned long long collatz(unsigned long long n) {
-    //if (n == 0) return 0; // it is an error but ok
     if (n == 1) return 1;
     else if (n == 2) return 2;
 
