81cf4f8822a4eb9bd4ba9733b5d62bf012ef706d
AgSmith95 <ivan.a.kornienko@gmail.com>
2022-05-26 23:39:11 +0300

    aads/next_permutation alg with iterators
    added algorithm with iterators
    checked that it gets the same results that std:: version

--- aads/next_perm/main.cpp
+++ aads/next_perm/main.cpp
@@ -2,6 +2,31 @@
 #include <vector>
 #include <algorithm>
 
+#include <cassert>
+
+template <typename It> // It - bidirectional iterator
+bool next_permutation(It first, It last)
+{
+	// Code taken from cppreference - https://en.cppreference.com/w/cpp/algorithm/next_permutation
+	// Explanation taken from geeksforgeeks - https://www.geeksforgeeks.org/lexicographically-next-permutation-in-cpp/
+
+	// For step 1. we need a "reversed" sequence
+	auto r_first = std::make_reverse_iterator(last);
+	auto r_last = std::make_reverse_iterator(first);
+	// 1. Traverse squence from the right and find the first element that
+	//    is not following the descending order
+	auto left = std::is_sorted_until(r_first, r_last);
+	if(left != r_last){
+		// find closest greater element to the element from step 1.
+		auto right = std::upper_bound(r_first, left, *left);
+		// 2. Swap element from 1. with the closest greater element
+		std::iter_swap(left, right);
+	}
+	// 3. Reverse what left of the sequence
+	std::reverse(left.base(), last);
+	return left != r_last;
+}
+
 // 0 1 2 5 3 3 0
 // 0 1 3 0 2 3 5
 template <typename T>
@@ -101,6 +126,22 @@ int main()
 		print_vect(next_permutation(v3));
 		print_vect(next_permutation(v3));
 		print_vect(next_permutation(v3));
+
+		v3 = {'F', 'A', 'D', 'E'};
+		std::vector<char> v3c1 = {'F', 'A', 'D', 'E'};
+		std::vector<char> v3c2 = {'F', 'A', 'D', 'E'};
+		while (std::next_permutation(v3c1.begin(), v3c1.end())) {
+			next_permutation(v3);
+			::next_permutation(v3c2.begin(), v3c2.end());
+
+			// DEBUG
+			// print_vect(v3c1);
+			// print_vect(v3c2);
+			// std::cout << '\n';
+
+			assert(v3c1 == v3c2);
+			assert(v3c1 == v3);
+		}
 
 		std::cout << '\n';
 	}
