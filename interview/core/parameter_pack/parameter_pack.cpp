72c410abeec8e49f85ab96443dc2a9926c097ef5
AgSmith95 <ivan.a.kornienko@gmail.com>
2022-11-06 02:24:51 +0200

    parameter_pack
    2nd type only

---
+++ interview/core/parameter_pack/parameter_pack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// [2] typename|class ... pack-name(optional)
+template <typename... Types>
+int sum_to_int(Types... args)
+{
+	return (int)(args + ...);
+}
+
+// I know nothing about other types of paramter packs.
+// TASK:
+// Find more info on [1], [3], [4], [5] and [6]
+
+int main()
+{
+	// sum_to_int(int, float, char)
+	std::cout << sum_to_int(5, 3.8f, 'a') << '\n'; // 105 // (int)(5 + 3.8 + 97)
+
+	return 0;
+}
